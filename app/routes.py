a74cd0834d0cbcf808f5df8e3d8699f2169aa6ce
mouse <arina021104@gmail.com>
2025-03-17 17:09:24 +0300

    lesson_5

--- app/routes.py
+++ app/routes.py
@@ -1,31 +1,15 @@
+from flask import render_template
 from app import app
 
-@app.route("/hello")
-def hello():
-    return "Hello, world!"
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-@app.route("/info")
-def info():
-    return "This is an informational page."
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
-@app.route("/calc/<int:a>/<int:b>")
-def calc(a, b):
-    return f"The sum of {a} and {b} is {a + b}."
-
-@app.errorhandler(404)
-def not_found(error):
-    return "Введите корректные целые числа.", 404
-
-@app.route("/reverse/<word>")
-def reverse(word):
-    if len(word) < 2:
-        return f"В слове 1 буква"
-    return f"{word} - {''.join(reversed(word))}"
-
-@app.route("/user/<name>/<int:age>")
-def user(name, age):
-    if age <= 0:
-        return "Возрост не может быть меньше 0"
-    return f"Hello, {name}. You are {age} years old."
-
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
